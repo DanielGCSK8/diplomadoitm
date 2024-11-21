873b17055f231855289504f74bcbb25dc337c4cb
Daniel Gomez <37761406-danielplus90@users.noreply.replit.com>
2024-11-21 01:21:46 +0000

    4th app

---
+++ docker/cuarta-aplicacion/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, send_file
+import boto3
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/voice', methods=['POST'])
+def voice():
+  try:
+    data = request.json
+    text = data['text']
+    Voice = 'Mia'
+    output_format = 'mp3'
+
+    if not text:
+      return jsonify({'error': 'No se recibió el texto'}), 400
+
+    polly = boto3.client('polly', region_name='us-east-1')
+
+    response = polly.synthesize_speech(
+        Text=text,
+        OutputFormat=output_format,
+        VoiceId=Voice,
+    )
+
+    audio_file = "audio.mp3"
+
+    with open(audio_file, 'wb') as file:
+      file.write(response['AudioStream'].read())
+
+    return send_file(audio_file, mimetype='audio/mpeg')
+
+  except Exception as e:
+    return jsonify({'error': str(e)}), 500
