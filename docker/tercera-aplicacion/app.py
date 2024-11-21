96febf49c0c7beeb53e3ce1df460bb8c01e0cb0a
Daniel Gomez <37761406-danielplus90@users.noreply.replit.com>
2024-11-21 00:14:14 +0000

    Get by id

--- docker/tercera-aplicacion/app.py
+++ docker/tercera-aplicacion/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import boto3
 
 app = Flask(__name__)
-dynamodb = boto3.resource('dynamodb', region_name='eu-east-1')
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 tabla = dynamodb.Table('tabla-danielgomez')
 
 
@@ -12,6 +12,20 @@ def index():
   item = {**data}
   tabla.put_item(Item=item)
   return 'Registro guardado exitosamente'
+
+
+@app.route('/get/<id>', methods=['GET'])
+def get_item(id):
+  try:
+    response = tabla.get_item(Key={'id': id})
+
+    if 'Item' in response:
+      return jsonify(response['Item']), 200
+    else:
+      return jsonify({'error': 'id no existe'}), 404
+
+  except Exception as e:
+    return jsonify({'error': str(e)}), 500
 
 
 if __name__ == "__main__":
